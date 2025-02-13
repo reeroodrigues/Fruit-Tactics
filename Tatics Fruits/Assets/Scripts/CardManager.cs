c13ea4df7b38425227cd18c1ab7ebcd8c11e6445
r-rodrigues <r.rodrigues@pipastudios.com>
2025-02-13 14:22:11 -0300

    Update to fix bugs

--- Tatics Fruits/Assets/Scripts/CardManager.cs
+++ Tatics Fruits/Assets/Scripts/CardManager.cs
@@ -1,27 +1,26 @@
 using System.Collections.Generic;
 using DefaultNamespace;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 public class CardManager : MonoBehaviour
 {
-    [FormerlySerializedAs("_cardType")] [HideInInspector] public CardTypeSo _cardTypeSo;
     [HideInInspector] public GameObject _selectedCard;
     [HideInInspector] public GameObject _hoveringMenu;
+    [HideInInspector] public CardTypeSo _cardType;
 
     [Header("Scripts/GameObjects")]
-    public Canvas _canvas;
-    public CardHolder _defaultPlayArea;
     public GameObject _cardParent;
-    public GameObject _cardFace;
-    public GameObject _singleCardsParent;
     public HorizontalLayoutGroup _defaultCardsLayoutGroup;
-    
+    public CardHolder _defaultPlayArea;
+    public GameObject _singleCardsParent;
+    public GameObject _cardFace;
+    public Canvas _canvas;
+
     [Header("Settings")]
-    [UnityEngine.Range(0,12)] public int _maxCards = 6;
-    [UnityEngine.Range(0,12)] public int _startingAmount = 6;
+    [Range(0,12)] public int _maxCards = 6;
+    [Range(0,12)] public int _startingAmount = 6;
 
     [Header("Lists")]
     public List<CardTypeSo> _cardTypes = new List<CardTypeSo>();
@@ -31,9 +30,8 @@ public class CardManager : MonoBehaviour
     {
         if (_startingAmount > 0)
             AddCard(_startingAmount);
-        
     }
-
+    
     private void Update()
     {
         HandleCardMovements();
@@ -55,7 +53,7 @@ public class CardManager : MonoBehaviour
                     break;
                 }
             }
-        
+
             if (_selectedCard.transform.position.x < _cards[i].transform.position.x)
             {
                 if (_selectedCard.transform.parent != null && _selectedCard.transform.parent.GetSiblingIndex() > _cards[i].transform.parent.GetSiblingIndex())
@@ -66,7 +64,7 @@ public class CardManager : MonoBehaviour
             }
         }
     }
-    
+
     public void PlayCard()
     {
         if (_selectedCard == null)
@@ -77,7 +75,7 @@ public class CardManager : MonoBehaviour
             var target = _selectedCard.transform.parent;
             _selectedCard.transform.position = _defaultPlayArea.transform.position;
             _selectedCard.transform.SetParent(_defaultPlayArea.transform);
-        
+
             var canvas = _selectedCard.GetComponent<Canvas>();
             if (canvas == null)
                 canvas = _selectedCard.AddComponent<Canvas>();
@@ -85,41 +83,22 @@ public class CardManager : MonoBehaviour
             canvas.overrideSorting = true;
             canvas.sortingOrder = GetNextSortingOrder();
 
-            // Remover a carta do CardVisuals
-            RemoveCardFace(_selectedCard.GetComponent<Card>());
-
             Destroy(target.gameObject);
             _selectedCard = null;
         }
     }
-    
-    private void RemoveCardFace(Card card)
-    {
-        GameObject cardVisualsParent = GameObject.Find("CardVisuals");
-        if (cardVisualsParent == null) return;
 
-        foreach (Transform child in cardVisualsParent.transform)
-        {
-            CardFace cardFace = child.GetComponent<CardFace>();
-            if (cardFace != null && cardFace._target == card.gameObject)
-            {
-                Destroy(child.gameObject);
-                break;
-            }
-        }
-    }
-    
     private int _sortingOrder = 1;
     private int GetNextSortingOrder()
     {
         return _sortingOrder++;
     }
-    
+
     private void SwapCards(GameObject currentCard, GameObject targetCard)
     {
         var currentCardParent = currentCard.transform.parent;
         var targetedCardParent = targetCard.transform.parent;
-        
+
         currentCard.transform.SetParent(targetedCardParent);
         targetCard.transform.SetParent(currentCardParent);
 
@@ -132,11 +111,9 @@ public class CardManager : MonoBehaviour
 
         GetComponent<AudioSource>().Play();
     }
-    
 
     public void AddCard(int amount)
     {
-        Debug.Log("Tentativa de adicionar nova carta.");
         for (int i = 0; i < amount; i++)
         {
             if (_defaultCardsLayoutGroup.transform.childCount < _maxCards)
@@ -144,10 +121,12 @@ public class CardManager : MonoBehaviour
                 var card = Instantiate(_cardParent, _defaultCardsLayoutGroup.transform);
                 var randomCard = Random.Range(0, _cardTypes.Count);
 
-                var cardComponent = card.GetComponentInChildren<Card>();
-                cardComponent._cardTypeSo = _cardTypes[randomCard];
+                card.GetComponentInChildren<Card>()._cardTypeSo = _cardTypes[randomCard];
+                var cardFace = Instantiate(_cardFace, GameObject.Find("CardVisuals").transform);
+
+                cardFace.GetComponent<CardFace>()._target = card.GetComponentInChildren<Card>().gameObject;
             }
         }
     }
-    
+
 }
