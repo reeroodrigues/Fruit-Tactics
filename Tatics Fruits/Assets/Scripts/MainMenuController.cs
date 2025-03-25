88b529df7e73b61881ee4eb1d9ac5fe659377f0d
r-rodrigues <r.rodrigues@pipastudios.com>
2025-03-24 23:11:38 -0300

    Inital MainMenu settings and UI

---
+++ Tatics Fruits/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,54 @@
+using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] private Button _playButton;
+    [SerializeField] private Button _shopButton;
+    [SerializeField] private Button _optionsButton;
+    [SerializeField] private Button _profileButton;
+    [SerializeField] private Transform _titleTransform;
+    [SerializeField] private SettingsMenuController _settingsMenu;
+    [SerializeField] private PlayerProfileController _playerProfileController;
+
+    private void Start()
+    {
+        _optionsButton.onClick.AddListener(_settingsMenu.OpenSettings);
+        _titleTransform.localScale = Vector3.zero;
+        _titleTransform.DOScale(1f, 0.8f).SetEase(Ease.OutBounce);
+
+        SetupButtonAnimation(_playButton);
+        SetupButtonAnimation(_shopButton);
+        SetupButtonAnimation(_optionsButton);
+        SetupButtonAnimation(_profileButton);
+
+        _playButton.onClick.AddListener(StartGame);
+        _shopButton.onClick.AddListener(OpenShop);
+        _optionsButton.onClick.AddListener(OpenOptions);
+        _profileButton.onClick.AddListener(OpenProfile);
+    }
+
+    private void SetupButtonAnimation(Button button)
+    {
+        button.transform.localScale = Vector3.one * 0.9f;
+        
+        var trigger = button.gameObject.AddComponent<EventTrigger>();
+
+        var pointerEnter = new EventTrigger.Entry { eventID = EventTriggerType.PointerEnter };
+        pointerEnter.callback.AddListener((eventData) => button.transform.DOScale(1f, 0.2f));
+        trigger.triggers.Add(pointerEnter);
+
+        var pointerExit = new EventTrigger.Entry { eventID = EventTriggerType.PointerExit };
+        pointerExit.callback.AddListener((eventData) => button.transform.DOScale(0.9f, 0.2f));
+        trigger.triggers.Add(pointerExit);
+    }
+
+    private void StartGame() => SceneManager.LoadScene("Gameplay");
+    private void OpenShop() => Debug.Log("Abrindo Loja...");
+    private void OpenOptions() => Debug.Log("Abrindo Opções...");
+    private void OpenProfile() => _playerProfileController.OpenProfile();
+}
---
+++ Tatics Fruits/Assets/Scripts/PlayerProfileController.cs
@@ -0,0 +1,90 @@
+using System.Text.RegularExpressions;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerProfileController : MonoBehaviour
+{
+    [SerializeField] private GameObject _profilePanel;
+    [SerializeField] private TextMeshProUGUI _playerNameText;
+    [SerializeField] private Image _avatarImage;
+    [SerializeField] private Sprite[] _avatars;
+    [SerializeField] private TMP_InputField _playerNameInput;
+    [SerializeField] private TextMeshProUGUI _playerNameErrorText;
+    [SerializeField] private GameObject _avatarSelectionPanel;
+    [SerializeField] private Button _closeAvatarPanelButton;
+
+    private int _currentAvatarIndex = 0;
+
+    private void Start()
+    {
+        // Carregar nome salvo
+        string savedName = PlayerPrefs.GetString("PlayerName", "Jogador");
+        _playerNameText.text = savedName;
+        _playerNameInput.text = savedName;
+
+        // Carregar avatar salvo
+        _currentAvatarIndex = PlayerPrefs.GetInt("AvatarIndex", 0);
+        _avatarImage.sprite = _avatars[_currentAvatarIndex];
+
+        // Adicionar listener para validar nome ao finalizar edição
+        _playerNameInput.onEndEdit.AddListener(ValidatePlayerName);
+
+        // Configurar botão de fechar painel de avatar
+        _closeAvatarPanelButton.onClick.AddListener(CloseAvatarSelection);
+    }
+
+    public void OpenProfile()
+    {
+        _profilePanel.SetActive(true);
+    }
+
+    public void CloseProfile()
+    {
+        _profilePanel.SetActive(false);
+    }
+
+    public void ChangeAvatar()
+    {
+        _currentAvatarIndex = (_currentAvatarIndex + 1) % _avatars.Length;
+        _avatarImage.sprite = _avatars[_currentAvatarIndex];
+        PlayerPrefs.SetInt("AvatarIndex", _currentAvatarIndex);
+        PlayerPrefs.Save();
+    }
+
+    public void OpenAvatarSelection()
+    {
+        _avatarSelectionPanel.SetActive(true);
+    }
+
+    public void CloseAvatarSelection()
+    {
+        _avatarSelectionPanel.SetActive(false);
+    }
+
+    public void SelectAvatar(int avatarIndex)
+    {
+        _currentAvatarIndex = avatarIndex;
+        _avatarImage.sprite = _avatars[_currentAvatarIndex];
+        PlayerPrefs.SetInt("AvatarIndex", _currentAvatarIndex);
+        PlayerPrefs.Save();
+        CloseAvatarSelection();
+    }
+
+    private void ValidatePlayerName(string name)
+    {
+        // Permitir apenas letras e espaços
+        if (!Regex.IsMatch(name, @"^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$"))
+        {
+            _playerNameErrorText.text = "Nome inválido! Apenas letras são permitidas.";
+            _playerNameInput.text = PlayerPrefs.GetString("PlayerName", "Jogador");
+        }
+        else
+        {
+            _playerNameErrorText.text = "";
+            _playerNameText.text = name;
+            PlayerPrefs.SetString("PlayerName", name);
+            PlayerPrefs.Save();
+        }
+    }
+}
