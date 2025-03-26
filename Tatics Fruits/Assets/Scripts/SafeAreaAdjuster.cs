22379eb5546b1453648eeeba6ca8ef24cada8b62
r-rodrigues <r.rodrigues@pipastudios.com>
2025-03-26 11:45:02 -0300

    Safe area and fixing cards problems

---
+++ Tatics Fruits/Assets/Scripts/SafeAreaAdjuster.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+public class SafeAreaAdjuster : MonoBehaviour
+{
+    private RectTransform _rectTransform;
+
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        ApplySafeArea();
+    }
+
+    private void ApplySafeArea()
+    {
+        var safeArea = Screen.safeArea;
+        var anchorMin = safeArea.position;
+        var anchorMax = safeArea.position + safeArea.size;
+
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+
+        _rectTransform.anchorMin = anchorMin;
+        _rectTransform.anchorMax = anchorMax;
+    }
+}
